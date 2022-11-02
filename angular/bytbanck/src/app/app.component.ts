fee9b2ef788787c6155489a79c4a9601b12ca649
Hélio Barros <barros.helio98@gmail.com>
2022-11-02 12:29:31 -0300

    Fechamento do tela de tranferencia

--- angular/bytbanck/src/app/app.component.ts
+++ angular/bytbanck/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+
+  transferencia: any;
+
+  transferir($event: any){
+    console.log($event);
+    this.transferencia = $event;
+
+  }
+
   title = 'bytbanck';
 }
--- angular/bytbanck/src/app/nova-transferencia/nova-transferencia.component.ts
+++ angular/bytbanck/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
 
@@ -9,8 +9,28 @@ import { Component } from "@angular/core";
 })
 
 export class NovaTransferenciaComponent {
-  tranferir(){
+
+
+  @Output()
+  aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
+  transferir(){
     console.log('Solicitando nova tranferencia');
 
+    //console.log('Valor: ', this.valor);
+    //console.log('Destino: ', this.destino);
+    const valorEmitir =  {valor: this.valor , destino: this.destino};
+    this.aoTransferir.emit( valorEmitir );
+
+    this.limparCampos();
+
+  }
+
+  limparCampos(){
+    this.destino = 0;
+    this.valor = 0;
   }
 }

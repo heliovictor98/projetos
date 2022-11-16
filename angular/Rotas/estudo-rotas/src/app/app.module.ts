260bd5a596c97faf16db44c102fe0e090cf71f5a
Hélio Barros <barros.helio98@gmail.com>
2022-11-15 22:47:36 -0300

    rotas

--- angular/Rotas/estudo-rotas/src/app/app.module.ts
+++ angular/Rotas/estudo-rotas/src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
